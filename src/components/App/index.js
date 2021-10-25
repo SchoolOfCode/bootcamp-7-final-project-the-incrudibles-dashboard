bd7ea2f24e708899df7783a0d20a16b075fb8150
diinin <diininasra94@gmail.com>
2021-10-25 14:16:36 +0100

    started on dashboard

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Login from "../App/../Login/index";
+import Login from "../Login/index";
+import Dashboard from "../Dashboard/index";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Dashboard />
     </div>
   );
 }
