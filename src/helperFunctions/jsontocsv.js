5e8f75add7006f65bcfcd5b6643baaaab24e7653
tomlowen <tom.r.lowen@googlemail.com>
2021-10-27 10:37:43 +0100

    refactored json function to handle nested data

--- src/helperFunctions/jsontocsv.js
+++ src/helperFunctions/jsontocsv.js
@@ -1,17 +1,3 @@
-// export default function convertJsonToCsv(jsonData) {
-//   let csvrecord = [Object.keys(jsonData.payload[0])].join(",") + "\n";
-
-//   jsonData.payload.forEach(function (jsonrecord) {
-//     csvrecord += Object.values(jsonrecord).join(",") + "\n";
-//   });
-
-//   return csvrecord;
-// }
-
-// set csv headers using dot notiaton to access the keys of the datat points that we want in the csv
-//for each graduate:
-//for each response:
-//create a new row, using  response data points + parent graduate info
 
 export default function convertJsonToCsv(jsonData) {
   let csvrecord =
