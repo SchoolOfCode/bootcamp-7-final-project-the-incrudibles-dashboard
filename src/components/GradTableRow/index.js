aba47ad1ebe21077360e6287fe54f2687505004e
tomlowen <tom.r.lowen@googlemail.com>
2021-10-29 15:10:49 +0100

    updated graduate view in table

--- src/components/GradTableRow/index.js
+++ src/components/GradTableRow/index.js
@@ -1,15 +1,12 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { getMostRecentResponse } from "../../helperFunctions/getrecentresponse";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import IconButton from "@mui/material/IconButton";
 import { useState } from "react";
-import { Box } from "@mui/system";
-import { Collapse, Typography } from "@mui/material";
+import { Collapse } from "@mui/material";
+import GradView from "../GradView";
 
 export default function GradTable({ gradData }) {
   const [open, setOpen] = useState(false);
@@ -17,7 +14,7 @@ export default function GradTable({ gradData }) {
 
   return (
     <>
-      <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
         <TableCell>
           <IconButton
             aria-label="expand row"
@@ -30,36 +27,14 @@ export default function GradTable({ gradData }) {
         <TableCell component="th" scope="row">
           {gradData.graduate_name}
         </TableCell>
-        <TableCell align="right">{gradData.cohort}</TableCell>
-        <TableCell align="right">{latestResponse.current_employer}</TableCell>
+        <TableCell align="left">{gradData.cohort}</TableCell>
+        <TableCell align="left">{latestResponse.current_employer}</TableCell>
         <TableCell align="right">{latestResponse.current_salary}</TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                Career information
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell> </TableCell>
-                    <TableCell> </TableCell>
-                    <TableCell align="right"> </TableCell>
-                    <TableCell align="right"> </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <p> </p>
-                  <p> </p>
-                  <p> </p>
-                  <p> </p>
-                  <p> </p>
-                  <p> </p>
-                </TableBody>
-              </Table>
-            </Box>
+            <GradView gradData={gradData} />
           </Collapse>
         </TableCell>
       </TableRow>
---
+++ src/components/GradView/index.js
@@ -0,0 +1,82 @@
+import Table from "@mui/material/Table";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Grid, Paper, TableBody, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { getMostRecentResponse } from "../../helperFunctions/getrecentresponse";
+
+export default function GradView({ gradData }) {
+  const latestResponse = getMostRecentResponse(gradData.responses);
+
+  return (
+    <Box sx={{ margin: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper sx={{ p: 2, display: "flex", flexDirection: "row" }}>
+            <Typography gutterBottom component="div">
+              {latestResponse.current_employer
+                ? `${
+                    gradData.graduate_name.split(" ")[0]
+                  } has been working as a ${
+                    latestResponse.current_position
+                  } at ${latestResponse.current_employer} for ${
+                    latestResponse.length_of_service
+                  }`
+                : `${
+                    gradData.graduate_name.split(" ")[0]
+                  } is currently not working in the technology sector`}
+            </Typography>
+          </Paper>
+        </Grid>
+        {/* selection */}
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: "content",
+            }}
+          >
+            <Table size="small" aria-label="purchases">
+              <TableHead>
+                <TableCell align="center">
+                  <Typography gutterBottom component="div">
+                    Survey responses
+                  </Typography>
+                </TableCell>
+              </TableHead>
+              <TableBody>
+                {gradData.responses.map((response) => (
+                  <TableRow>
+                    <TableCell align="center">
+                      {response.timestamp.slice(0, 10)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+        </Grid>
+        {/* pie chart */}
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              height: 240,
+            }}
+          ></Paper>
+        </Grid>
+        {/* all graduates */}
+        <Grid item xs={12}>
+          <Paper
+            sx={{ p: 2, display: "flex", flexDirection: "column" }}
+          ></Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
